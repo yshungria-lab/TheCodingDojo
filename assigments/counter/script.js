b70df8b487c8fad63e002873137c62d0a176df8e
Yojan Stiben Hungría <69788548+yshungria-lab@users.noreply.github.com>
2022-05-30 13:04:47 -0500

    My Epic Counter

---
+++ assigments/counter/script.js
@@ -0,0 +1,15 @@
+let count = 1;
+countElement = document.querySelector("#count");
+console.log(countElement);
+
+function add1() {
+  count++;
+  countElement.innerText = "The count is " + count;
+  console.log(count);
+}
+
+function subtract1() {
+  count--;
+  countElement.innerText = "The count is " + count;
+  console.log(count);
+}
